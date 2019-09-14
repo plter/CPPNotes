a0f8fcf28b46a6579d08931114c2ac3e70b9f07f
plter <xtiqin@163.com>
2019-09-14 15:58:05 +0800

    理解C++异步编程

---
+++ N014CoAsync/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <boost/asio.hpp>
+
+using namespace std;
+
+boost::asio::awaitable<int> async_add(int a, int b) {
+    co_return a + b;
+}
+
+
+boost::asio::awaitable<void> asyncMain() {
+    auto d = co_await async_add(10, 1);
+    std::cout << d << std::endl;
+    co_return;
+}
+
+
+int main() {
+    boost::asio::io_context io_context;
+
+    boost::asio::co_spawn(io_context, asyncMain, [](auto err) {
+        std::cout << "OK 1" << std::endl;
+    });
+    io_context.run();
+    return 0;
+}
